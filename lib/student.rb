ea042bb4996b2a6102c9ab6834b305760a5e5973
Ranika Williams <rnkprice07@gmail.com>
2019-08-11 17:29:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,14 +10,14 @@ class Student
   end
 
   def self.create_table
-    sql = <<_SQL
+    sql = <<-SQL
       CREATE TABLE students(
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
       )
       SQL
-   DB[:conn].execute(sql)  
+   DB[:conn].execute(sql)
   end
 
 end
